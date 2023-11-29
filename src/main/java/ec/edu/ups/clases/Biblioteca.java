f922f6a9347d0cfa890865c14304745407ffa255
Karen Ortiz <karenorpe2004@gmail.com>
2023-11-29 17:50:51 -0500

    Implementacion de la relacion de agregacion entre las clases Biblioteca con Usuario y Libro, modificación de constructures e implementación de sus métodos

--- src/main/java/ec/edu/ups/clases/Biblioteca.java
+++ src/main/java/ec/edu/ups/clases/Biblioteca.java
@@ -1,20 +1,32 @@
 package ec.edu.ups.clases;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Biblioteca {
 	private String nombre;
 	private String direccion;
+	private ArrayList <Libro> listaLibros;
+	private ArrayList <Usuario> listaUsuarios;
 	
 	//Constructor
+
 	public Biblioteca(String nombre, String direccion) {
+		super();
 		this.nombre = nombre;
 		this.direccion = direccion;
+		this.listaLibros = new ArrayList();
+		this.listaUsuarios = new ArrayList();
 	}
 	
+	
+
 	public Biblioteca() {
-		
+		this.listaLibros = new ArrayList();
+		this.listaUsuarios = new ArrayList();
 	}
+
+
 
 	//Getters y Setters
 	public String getNombre() {
